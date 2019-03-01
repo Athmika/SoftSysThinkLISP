70918d96cd2728a37de76e450b42ab0c947f90b7
Athmika <athmikas@gmail.com>
2019-02-28 19:20:42 -0500

    Added error checking to the prompt + removes leading spaces from inputs

--- prompt.c
+++ prompt.c
@@ -10,34 +10,65 @@ Implemnting an interactive prompt.
 int main(int argc, char** argv) {
 
   /* Print Version and Exit Information */
- char *inputs[10];
+ char *inputs[101];
 
   printf("lisp> ");
-  char s[100];
-  char input[100];
+  char s[201];
+  char input[201];
   int numInputs = 0;
   int i = 0;
+
+//This loop goes through all the lines of input until a blank line is entered
 
   while (1 == scanf("%[^\n]%*c", s))
   {
     printf("lisp> ");
-    //Error checking to make sure that input size is not greater than 100
+    //Error checking to make sure that command size is not greater than 200
+    if (strlen(s)>201)
+    {
+      printf("Your command cannot be more than 200 characters\n");
+      exit(1);
+    }
     inputs[i] = malloc(100 * sizeof(char));
-    strcpy(inputs[i],s);
+
+    char *str_ptr = s;
+    int x = 0;
+
+    //The loop removes the leading spaces
+    for (x=0;x<strlen(s);x++)
+    {
+      if(isspace(s[x]))
+      {
+        str_ptr = str_ptr + 1;
+      }
+      else
+      {
+        break;
+      }
+    }
+
+    strcpy(inputs[i],str_ptr);
 
     numInputs++;
     i++;
+
+    //Error checking to make sure that the number of commands is not greater than 100
+    if (numInputs > 100)
+    {
+        printf("You cannot enter more than 100 commands\n");
+        exit(1);
+    }
+
   }
 
   i = 0;
+  //Loop prints out all the inputs
   while(numInputs>0)
   {
       printf("%s\n", inputs[i]);
        i++;
        numInputs--;
   }
-
-  printf("Number of inputs = %i\n", numInputs);
 
 
   return 0;
