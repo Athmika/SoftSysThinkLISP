30364f708f3d74ffb5f4b151084db02c7784c918
Abby Fry <abbymfry@gmail.com>
2019-02-28 19:59:01 -0500

    updates to parser.c

--- parser.c
+++ parser.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//http://leohetsch.com/create-a-minimal-lisp-like-interpreter-in-c/
+
 #define ADD '+'
 #define SUB '-'
 #define MUL '*'
@@ -8,24 +10,40 @@
 
 char* read_input(){
 //function reads user input and returns pointer to the input string
-  char* input=malloc(256);
-  fgets(input, 256, stdin);
+  char* input=malloc(25600);
+  fgets(input, 25600, stdin);
+  printf("%s\n",input);
   return input;
 }
 
 int evaluate(char* input){
+  printf("Hello Hyegi1");
   //function does numerical evaluation
   if (*(input++) != '('){ //check value of input, check for open paren, increment by 1
     printf("ERROR: Input must begin with a '('\n");
+    exit(1);
   }
-  char* operator = *(input++); //read the operator
 
+  printf("Hello Hyegi2");
+  *input = *(input++)
+  char* operator = *input; //read the operator
   int result = 0;
   switch (operator) {
     case ADD:
       result = compute_ADD(input);
       break;
-    default: break;
+    case MUL:
+      result = compute_MUL(input);
+      break;
+    case DIV:
+      result = compute_DIV(input);
+      break;
+    case  SUB:
+      result = compute_SUB(input);
+      break;
+    default:
+      printf("ERROR--NO OPERATOR PROVIDED");
+      break;
   }
 }
 
@@ -37,23 +55,70 @@ int compute_ADD(char* input){
       input++;
       continue;
     }
-    int a = 0;
-    scanf(input++, "%d",&a);
+    int a = atoi(input);
+    printf("hello")
+    // scanf(input++, "%d",&a);
     sum += a;
   }
   return sum;
 }
 
-int main(int argc, char*argv[]){
-  while (1){
-    evalute(read_input());
-  }
+int compute_MUL(char* input) {
 
-  return 0;
+    int product = 1; // 1 is neutral for product
+
+    while (*input != ')') {
+        if (*input == ' ') {
+            input++; continue;
+        }
+
+        int m = 0;
+        sscanf(input++, "%d", &m);
+        product *= m;
+    }
+
+    return product;
 }
 
+int compute_DIV(char* input) {
+    int  t= 0;
+    int n = 0;
+
+    while(*(++input) == ' ');
+
+    // first value found
+    sscanf(read_input, "%d", &t);
+    ++input;
+
+    while(*(++input) == ' ');
+
+    // second value found
+    sscanf(input, "%d", &n);
+
+    return t / n;
+}
+
+int compute_SUB(char* input){
+  int  f= 0;
+  int l = 0;
+
+  while(*(++input) == ' ');
+
+  // first value
+  sscanf(read_input, "%d", &f);
+  ++input;
+
+  while(*(++input) == ' ');
+
+  // second value
+  sscanf(input, "%d", &l);
+  return f-l;
+}
 
 int main(int argc, char* argv[]) {
+  while (1) {
+    printf("-> %d", evaluate(read_input()));
+}
 
     return 0;
 }
